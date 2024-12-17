5292040e4714337161f2e4c5cb2e754532dc7723
Văn Thiết ( Frontend DEV ) <125172255+vanthiet1@users.noreply.github.com>
2024-12-17 20:22:55 +0700

    Update http.js

--- Client/src/utils/helpers/http.js
+++ Client/src/utils/helpers/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const http = axios.create({ 
-    baseURL: 'https://datn-quanlinhathuoc.onrender.com',
+    baseURL: 'https://datn-quanlinhathuoc.onrender.com/api/v1',
     headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'multipart/form-data' 
